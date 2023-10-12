6f79a902b9a25a566bd42b70f42077fb55b2b1e5
diamondstorm-leaderofchickenresistance <143503714+diamondstorm-leaderofchickenresistance@users.noreply.github.com>
2023-10-12 17:23:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,13 +9,12 @@ document.getElementByID('P2').style.backgroundImage = "url('" + selectedImage + 
 function Score() {
 
 };
-function InduceRageUnfairly() {
-  if int(Score()) > 10 {
+function induceRageUnfairly() {
+  if Score() > 10 {
   Math.floor(math.random()*20)+1;
   let unlucky = document.createElement('p');
-  let unluckytext = document.createTextNode('So, you\'ve not lost 10 times with random chance huh? That has a 1 in 0.00001693508 chance of happening, so I\'ll assume you cheated. Do you want to go to the moon?');
-  unlucky.appendChild(unluckytext);
+  let unluckyText = document.createTextNode('So, you\'ve not lost 10 times with random chance huh? That has a 1 in 0.00001693508 chance of happening, so I\'ll assume you cheated. Do you want to go to the moon?');
+  unlucky.appendChild(unluckyText);
   let element = document.getElementById('vssection');
   element.appendChild(unlucky);
   };
-};
