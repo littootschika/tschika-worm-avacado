e233c38f30225cb58d787de2186fe46c79a7eefe
diamondstorm-leaderofchickenresistance <143503714+diamondstorm-leaderofchickenresistance@users.noreply.github.com>
2023-10-12 16:54:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 function NPC() {
 const images = [
-  "docs/assets/tschika p1.png",
   "docs/assets/tschika p2.png",
 ];
-var randomIndex = Math.floor(Math.random() * images.length);
+var randomIndex = Math.floor(Math.random() * images.length) +1;
 var selectedImage = images[randomIndex];
 document.getElementByID('P2').style.backgroundImage = "url('" + selectedImage + "')";
-};
 };
 function Score() {
 
