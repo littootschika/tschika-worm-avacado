58b4175915711a1c495f3f25a700ebbfd320a6e6
diamondstorm-leaderofchickenresistance <143503714+diamondstorm-leaderofchickenresistance@users.noreply.github.com>
2023-10-16 16:35:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 function NPC() {
-const images = [
-  "docs/assets/tschika p2.png",
-];
-var randomIndex = Math.floor(Math.random() * images.length) +1;
-var selectedImage = images[randomIndex];
-document.getElementByID('P2').style.backgroundImage = "url('" + selectedImage + "')";
-};
-function Score() {
-
+    const images = [
+        "docs/assets/tschika p2.png",
+    ];
+    var randomIndex = Math.floor(Math.random() * images.length) +1;
+    var selectedImage = images[randomIndex];
+    document.getElementByID('P2').style.backgroundImage = "url('" + selectedImage + "')";
+    };
+function ChangeBackground() {
+    const images = [
+        "docs/assets/turtle head and worm body soup.png",
+    ];
+    var randomIndex = Math.floor(Math.random() * images.length) +1;
+    var selectedImage = images[randomIndex];
+    document.getElementByID('body').style.backgroundImage = "url('" + selectedImage + "')";
+    };
+    
+function score() {
+    
 };
 function induceRageUnfairly() {
-  if Score() > 10 {
-  Math.floor(math.random()*20)+1;
-  let unlucky = document.createElement('p');
-  let unluckyText = document.createTextNode('So, you\'ve not lost 10 times with random chance huh? That has a 1 in 0.00001693508 chance of happening, so I\'ll assume you cheated. Do you want to go to the moon?');
-  unlucky.appendChild(unluckyText);
-  let element = document.getElementById('vssection');
-  element.appendChild(unlucky);
-  };
-// the stuff that the player chooses
-const p1Image = document.getElementById('P1');
-  
-function chooseTschika() {
-  p1Image.setAttribute('src','docs/assets/tschika p1.png');
-};
+      if score() > 10 {
+      Math.floor(math.random()*20)+1;
+      let unlucky = document.createElement('p');
+      let unluckyText = document.createTextNode('So, you have not lost 10 times with random chance huh? That has a 1 in 0.00001693508 chance of happening, so you probably cheated. Do you want to go to the moon?');
+      unlucky.appendChild(unluckyText);
+      let element = document.getElementById('vssection');
+      element.appendChild(unlucky);
+      document.getElementsByTagName('body').style.display = 'none';
+    };
